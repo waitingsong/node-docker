3e3716e34347a09151385bbf5e0fdfebfd68d6b2
waiting <1661926154@qq.com>
2020-12-07 00:21:57 +0800

    feat: install v14 from @edge branch

---
+++ scripts.d/alpine-files-14.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+
+
+echo '#!/bin/sh
+set -e
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
+exec "$@"' > ./docker-entrypoint.sh
+
+
+cat >./Dockerfile <<EOL
+# https://stackoverflow.com/a/43743532
+# Without NPM
+
+FROM alpine:3.12
+
+ENV NODE_VERSION $IMAGE_VER
+ENV ENV "/root/.ashrc"
+
+# Mirrors
+# http://dl-cdn.alpinelinux.org/alpine/MIRRORS.txt
+# https://uk.alpinelinux.org/alpine/
+# https://mirrors.tuna.tsinghua.edu.cn/alpine/
+# https://mirrors.sjtug.sjtu.edu.cn/alpine/
+# https://mirrors.ustc.edu.cn/alpine/
+# https://mirrors.nju.edu.cn/alpine/
+# https://mirror.lzu.edu.cn/alpine/
+
+COPY docker-entrypoint.sh /usr/local/bin/
+
+RUN chmod a+x /usr/local/bin/docker-entrypoint.sh \\
+  && cat /etc/alpine-release \\
+  && NODE_DIST="https://npm.taobao.org/dist" \\
+  && MIRROR="https://mirrors.tuna.tsinghua.edu.cn" \\
+  && sed -i "s#http://dl-cdn.alpinelinux.org#\$MIRROR#g" /etc/apk/repositories \\
+  && echo "@edge \$MIRROR/alpine/edge/main" >> /etc/apk/repositories \\
+  && addgroup -g 1000 node \\
+  && adduser -u 1000 -G node -s /bin/sh -D node \\
+  && apk add --no-cache curl nodejs@edge=~\$NODE_VERSION \\
+  && ln -s /usr/bin/node /usr/bin/nodejs \\
+  && node --version \\
+  # && sed -i "s#:/bin/ash#:/bin/bash#g" /etc/passwd \\
+  && echo "alias crontab='crontab -i'; \\
+    alias ll='ls -l --color=auto'; \\
+    alias time='/usr/bin/time '; \\
+    alias tarz='tar -I zstdmt'; \\
+    export XZ_DEFAULTS='-T 0 -4'; \\
+    export ZSTD_CLEVEL=9; \\
+    " > \$ENV \\
+  && echo "export XZ_DEFAULTS='-T 0 -4'; \\
+    export ZSTD_CLEVEL=9; \\
+    " >> /etc/profile \\
+  && rm /var/cache/apk/* /var/lib/apt/lists/* /tmp/* /var/tmp/* -rf \\
+  && mkdir -p /app
+
+WORKDIR /app
+ENTRYPOINT ["docker-entrypoint.sh"]
+CMD [ "node" ]
+
+EOL
+
---
+++ scripts.d/create-alpine-14.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+IMAGE_VER="14.15.1"
+IMAGE_TAG=alpine
+TAGS="$IMAGE_VER-$IMAGE_TAG"
+BR="image-$TAGS"
+
+
+if [[ ! -z ${IMAGE_VER} ]]; then
+
+  echo -e 'First: Check specify version exists on https://pkgs.alpinelinux.org/packages?name=nodejs&branch=edge&repo=main \n'
+
+  while true; do
+    echo -n "[Question] "; read -e -p "Tag '$TAGS'. Do you want to continue? [y|n] " -i "" yn
+    case ${yn} in
+      [Nn]*) exit 1;;
+      [Yy]*) break;;
+      *)     echo 'Please answer (y)es or (n)o';;
+    esac;
+  done;
+
+  source scripts.d/alpine-files-14.sh
+
+  rm scripts.d -rf
+  git checkout --orphan "$BR"
+  git rm -rf .
+  git add .
+  git commit -m "feat: create Dockerfile with tag: $TAGS"
+  git push -f --set-upstream origin "$BR"
+  git checkout master
+else
+  echo '[ERR] Tag name not passed. Tag should looks like "14.15.1" (without any prefix or postfix)'
+fi
